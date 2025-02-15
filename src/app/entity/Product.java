c095c33acd5c9ccef0c2963dcfa8a7db1a6a89e6
undefinedDen <tunevdenis123@gmail.com>
2025-02-15 11:25:29 +0200

    Create product

---
+++ src/app/entity/Product.java
@@ -0,0 +1,46 @@
+package app.entity;
+
+public class Product {
+    private  String name;
+    private  int quota;
+    private  double price;
+
+    public Product(String name, int quota, double price) {
+        this.name = name;
+        this.quota = quota;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuota() {
+        return quota;
+    }
+
+    public void setQuota(int quota) {
+        this.quota = quota;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", quota=" + quota +
+                ", price=" + price +
+                '}';
+    }
+}
